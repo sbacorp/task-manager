2cccd4e64e15c6105448bd54da0ec0d881ac5d96
shankin <shankin.dev@gmail.com>
2023-03-25 12:06:32 +0300

    123

--- src/store/slices/selectors.ts
+++ src/store/slices/selectors.ts
@@ -1,5 +1,5 @@
 import { RootState } from "../";
 import { IBoard } from "./types";
-export const BoardsSelector = (state: RootState) => state.boards;
-export const BoardItemSelector = (id: string) => (state: RootState) =>
-	state.boards.boards.find((obj:IBoard) => obj.id === id);
+// export const BoardsSelector = (state: RootState) => state.boards;
+// export const BoardItemSelector = (id: string) => (state: RootState) =>
+// 	state.boards.boards.find((obj:IBoard) => obj.id === id);
