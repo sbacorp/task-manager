575e83fb920f6d6625108ba5dfa6111f5b0393b9
Bogdan <shankin.dev@gmail.com>
2023-04-26 22:43:14 +0300

    added react-hook-forms to registration

--- src/pages/auth/registration.tsx
+++ src/pages/auth/registration.tsx
@@ -3,39 +3,39 @@ import SignLayout from "@/components/signLayout";
 import supabase from "../../lib/supabaseClient";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { useForm,SubmitHandler } from "react-hook-form";
+import { useForm } from "react-hook-form";
 import { toast } from "react-toastify";
 
-function Registration() {
+interface IFormInput {
+	userName: string;
+	password: string;
+	email: string;
+}
+
+export default function Registration() {
 	const {
 		register,
 		handleSubmit,
 		formState: { errors },
-	} = useForm();
+	} = useForm<IFormInput>({});
 	const router = useRouter();
-	const [email, setEmail] = useState("");
-	const [password, setPassword] = useState("");
-	const [userName, setUserName] = useState("");
-	const [message, setMessage] = useState("");
-interface IFormInput {
-	userName: string;
-	password: string;
-	email:string
-}
-	const handleSignUp:SubmitHandler<IFormInput> = async (data) => {
+	console.log(errors, "errors");
+	const handleSignUp = async (data: IFormInput) => {
+		console.log(data);
+
 		const { data: user, error } = await supabase.auth.signUp({
-			email:data.email,
-			password:data.password,
+			email: data.email,
+			password: data.password,
 		});
 		if (user.user) {
-			const {  error } = await supabase.from("profiles").insert([
+			const { error } = await supabase.from("profiles").insert([
 				{
 					id: user.user.id,
 					userName: data.userName,
 					email: user.user.email,
 				},
 			]);
-			if(error){
+			if (error) {
 				toast.error(error.message);
 			}
 		}
@@ -46,7 +46,6 @@ interface IFormInput {
 			setTimeout(() => router.push("/auth/login"), 1000);
 		}
 	};
-	console.log(errors)
 	return (
 		<SignLayout>
 			<div className="flex flex-col items-center gap-7 font-serif">
@@ -58,67 +57,64 @@ interface IFormInput {
 					<div className="flex flex-col gap-3">
 						<p className="text-dark">Никмейм</p>
 						<input
-							{...(register("userName"),
-							{
-								required: true,
-								minLength: 4,
+							{...register("userName", {
+								required: "Поле обязательно для заполнения",
+								minLength: {
+									value: 4,
+									message: "Минимум 4 символа",
+								},
+								maxLength: {
+									value: 15,
+									message: "Максимум 15 символов",
+								},
 							})}
 							className="block py-1.5 px-3 w-full bg-dark7 text-gray0 lg:w-96 rounded"
 							type="text"
-							value={userName}
-							onChange={(e) => setUserName(e.target.value)}
 						/>
+
+						{errors.userName && (
+							<p className="text-white text-base">{errors.userName.message}</p>
+						)}
 					</div>
 					<div className="flex flex-col gap-3">
 						<p className="text-dark">Электроннаяя почта</p>
 						<input
-							{...(register("email"),
-							{
-								required: true,
+							{...register("email", {
+								required: "Поле обязательно для заполнения",
 							})}
 							className="block py-1.5 px-3 w-full bg-dark7 text-gray0 lg:w-96 rounded"
 							type="email"
-							value={email}
-							onChange={(e) => setEmail(e.target.value)}
-							required
 						/>
+						{errors.email && (
+							<p className="text-white text-base">{errors.email.message}</p>
+						)}
 					</div>
 					<div className="flex flex-col gap-3">
 						<p className="text-dark">Пароль</p>
 						<input
-							{...(register("password"),
-							{
-								required: true,
-								minLength: 8,
-							})}
+							{...register("password")}
 							className="block py-1.5 px-3 w-full bg-dark7 text-gray0  rounded lg:w-96"
 							type="password"
-							value={password}
-							onChange={(e) => setPassword(e.target.value)}
-							required
 						/>
 					</div>
 					<div className="flex gap-1 items-center">
 						<p className="text-dark2 font-normal text-base">
 							Уже есть аккаунт?
 						</p>
-						<Link className="text-dark font-normal text-base" href="/auth/login">
+						<Link
+							className="text-dark font-normal text-base"
+							href="/auth/login"
+						>
 							Войти
 						</Link>
 					</div>
-					<button
+					<input
 						type="submit"
-						className=" self-center flex items-center rounded w-fit justify-center bg-dark6 text-white font-semibold text-sm px-4 py-2"
-					>
-						Создать аккаунт
-					</button>
+						value="Создать аккаунт"
+						className="cursor-pointer self-center flex items-center rounded w-fit justify-center bg-dark6 text-white font-semibold text-base px-4 py-2"
+					/>
 				</form>
-				{message && (
-					<p className="text-white absolute bottom-10 left-1/2">{message}</p>
-				)}
 			</div>
 		</SignLayout>
 	);
 }
-
-export default Registration;
